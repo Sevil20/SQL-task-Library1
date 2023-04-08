ff976c7a5df0db0b767f205c5f93c7b6616c4201
Sevil Mirzayeva <114071633+Sevil20@users.noreply.github.com>
2023-04-08 12:43:38 +0300

    Add files via upload

---
+++ SQLQuery2 Library1.sql
@@ -0,0 +1,13 @@
+﻿use DbLibrary
+Select BookAuthor, count(BookName) as 'Kitab Sayi' from Books group by BookAuthor
+select * from Books
+Select BookPublisher, SUM(BookPrice) as 'Kitab Qiymetleri' from Books group by BookPublisher
+select BookPublisher, MIN(BookPrice) as 'Ən Ucuz', MAX(BookPrice) as 'Ən Bahalı' from Books group by BookPublisher
+select BookPublisher, COUNT(BookName) as 'Kitab Sayısı' 
+from Books group by BookPublisher having COUNT(BookName)>2
+select BookPublisher, SUM(BookPrice) as 'Kitab Gəliri',
+SUM(Tax) as 'Toplam Vergi',
+(SUM(BookPrice)-SUM(Tax)) as 'Toplam Gəlir'
+from Books group by BookPublisher
+having (SUM(BookPrice)-SUM(Tax))<50
+
